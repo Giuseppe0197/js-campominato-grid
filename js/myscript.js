5cf56e70dcf26534985ef3b350cd398b534f90e9
Giuseppe0197 <faro.g@hotmail.it>
2021-11-02 14:29:24 +0100

    percorso logico js e impostazione html e css

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+/* L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49 
+Quando l’utente clicca su ogni cella, la cella cliccata si colora*/
+
+
+/* creiamo le variabili necessarie per i bottoni in pagina che genereranno le celle */
+
+/* creiamo la variabile per il contenitore delle celle */
+
+/* creiamo una funzione comune per generare le celle in base al livello */
+
+/* andiamo a riportare la funzione e a creare il numero di celle necessarie in base al livello scelto dall'utente */
+
+/* andiamo a generare i numeri all'interno delle celle */
+
+/* aggiungiamo la funzione di click che va a colorare la cella che viene selezionata dall'utente */
